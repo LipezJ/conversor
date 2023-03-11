ec5737ffaf2286c309761d4d69530b01aabde1f3
LipezJ <juandavidlipez47@gmail.com>
2023-03-11 16:34:31 -0500

    conversor no lineal

---
+++ ConversorNoLineal/FuncionesNoLineales.java
@@ -0,0 +1,52 @@
+package ConversorNoLineal;
+
+import Utilidades.*;
+import java.util.*;
+
+import java.util.function.Function;
+
+public class FuncionesNoLineales {
+    private ArrayList<ArrayList<Function<Double, Double>>> tasas = new ArrayList<ArrayList<Function<Double, Double>>>();
+
+    public Function<Double, Double> getTasa(Unidad unidadOrigen, Unidad unidadDestino) {
+        if (unidadOrigen.getIdentificador() >= 0 && unidadOrigen.getIdentificador() < this.tasas.size()
+            && unidadDestino.getIdentificador() >= 0 && unidadDestino.getIdentificador() < this.tasas.size()) {
+            return this.tasas.get(unidadOrigen.getIdentificador()).get(unidadDestino.getIdentificador());
+        } else {
+            throw new IllegalArgumentException("Identificador de unidad no válido");
+        }
+    }
+
+    public void setTasas(ArrayList<Function<Double, Double>> nuevasTasas, ArrayList<Function<Double, Double>> nuevasInversas){
+        if (nuevasTasas.size() == tasas.size()) {
+            for (int i = 0; i < this.tasas.size(); i++) {
+                this.tasas.get(i).add(nuevasInversas.get(i));
+            }
+            nuevasTasas.add(valor -> valor);
+            this.tasas.add(nuevasTasas);
+        } else {
+            throw new IllegalArgumentException("Lista de tasas no valida");
+        }
+    }
+
+    public void setTasa(Unidad unidadOrigen, Unidad unidadDestino, Function<Double, Double> nuevaTasa) {
+        if (unidadOrigen.getIdentificador() >= 0 && unidadOrigen.getIdentificador() < this.tasas.size()
+            && unidadDestino.getIdentificador() >= 0 && unidadDestino.getIdentificador() < this.tasas.size()) {
+            this.tasas.get(unidadOrigen.getIdentificador()).set(unidadDestino.getIdentificador(), nuevaTasa);
+        } else {
+            throw new IllegalArgumentException("Identificador de unidad no válido");
+        }
+    }
+
+    public void agregarTasa(ArrayList<Function<Double, Double>> nuevasTasas) {
+        if (nuevasTasas.size() == this.tasas.size()) {
+            this.tasas.add(nuevasTasas);
+        } else {
+            throw new IllegalArgumentException("El tamaño de la lista de this.tasas no coincide con el número de unidades");
+        }
+    }
+
+    public int getSize(){
+        return this.tasas.size();
+    }
+}
---
+++ ConversorNoLineal/UnidadesNoLineales.java
@@ -0,0 +1,31 @@
+package ConversorNoLineal;
+
+import java.util.*;
+import java.util.function.Function;
+
+import Utilidades.Unidad;
+
+public class UnidadesNoLineales {
+    private Map<String, Unidad> unidades = new HashMap<String, Unidad>();
+    private FuncionesNoLineales tasas = new FuncionesNoLineales();
+
+    public boolean agregarUnidad(String nombre, ArrayList<Function<Double, Double>> tasas, ArrayList<Function<Double, Double>> nuevasInversas) {
+        this.unidades.put(nombre, new Unidad(nombre, this.tasas.getSize()));
+        this.tasas.setTasas(tasas, nuevasInversas);
+        return true;
+    }
+
+    public double cambiar(String origen, String destino, double cantidad) {
+        Unidad o = this.unidades.get(origen);
+        Unidad d = this.unidades.get(destino);
+        if (o == null || d == null) {
+            throw new IllegalArgumentException("Unidad no encontrada.");
+        }
+        Function<Double, Double> function = this.tasas.getTasa(o, d);
+        return function.apply(cantidad);
+    }
+
+    public Set<String> getUnidades(){
+        return this.unidades.keySet();
+    }
+}
